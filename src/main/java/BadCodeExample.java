64d0b8d07792d88f73f41a3a318ecf0a1a100dc4
Anastasiya Skopich <anastasiya.skopich@gmail.com>
2018-11-19 18:38:23 +0200

    Home Task  #3

--- src/main/java/BadCodeExample.java
+++ src/main/java/BadCodeExample.java
@@ -11,16 +11,29 @@ public class BadCodeExample {
         System.out.println("Hello world!!!");
 
         WebDriver webDriver = new ChromeDriver();
+        String searchTerm = "Selenium";
         webDriver.get("https://www.google.com/");
 
         WebElement element = webDriver.findElement(By.name("q"));
-        element.sendKeys("Selenium");
+        element.sendKeys("searchTerm");
         element.sendKeys(Keys.ENTER);
 
         List<WebElement> resultsList = webDriver.findElements(By.xpath("//div[@class='srg']/div[@class='g']"));
+
         System.out.println(resultsList.size());
 
+        //for each WebElement in list of WebElement print Text.
 
+        for (WebElement result : resultsList) {
+            String resultText = result.getText();
+            System.out.println(resultText);
+
+            if (resultText.toLowerCase().contains(searchTerm.toLowerCase())) {
+                System.out.println("searchTerm " + searchTerm + " found in block:\n" + resultText);
+            } else {
+                System.out.println("searchTerm " + searchTerm + " NOT found in block:\n" + resultText);
+            }
+        }
 
 
     }
